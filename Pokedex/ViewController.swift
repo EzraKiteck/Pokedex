c9488a2037d0846f6f37ed67c3e2c00bd3a34dbe
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-12 12:26:30 -0600

    Set up UI, and text field accepts pokemon ID or name

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    //IB Outlets
+    @IBOutlet weak var pokemonNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var pokemonName = ""
+    
+    //Base URL for request
+    let pokemonAPIBaseURL = "http://pokeapi.co/api/v2/item/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        //Takes Keyboard away
+        pokemonNameTextField.resignFirstResponder()
+        
+        //Checks against the Pokemon name being nil
+        if pokemonNameTextField.text != nil || Int(pokemonNameTextField.text!) != nil {
+            pokemonName = pokemonNameTextField.text!
+        }  else {
+            print("Please fill all text fields.")
+            return
+        }
+        
+        //Clears text field
+        pokemonNameTextField.text = ""
+        
+        print(pokemonName)
+    }
+    
 
 }
 
