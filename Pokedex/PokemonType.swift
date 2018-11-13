a30d813f948e46cbffd01029ceb0d4aeb0ab71f7
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-13 14:36:31 -0600

    Gets the first type of a pokemon and changes the bg color accordingly

---
+++ Pokedex/PokemonType.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonType.swift
+//  Pokedex
+//
+//  Created by Ezra Kiteck on 11/13/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import UIKit
+
+class PokemonType: NSObject {
+    
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -17,10 +17,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var pokemonNameTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet var mainView: UIView!
     
     //Base URL for request
     let pokemonAPIBaseURL = "https://pokeapi.co/api/v2/pokemon/"
     let imageAPIBaseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    let typeAPIBaseURL = "https://pokeapi.co/api/v2/type/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,17 +52,79 @@ class ViewController: UIViewController {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                //To print all json, use String(describing: json)
                 //Displays the name and ID of the pokemon
                 self.descriptionTextView.text = "Name: \(json["name"])\nID: \(json["id"])"
                 //Uses the base URL + the pokemons ID# + .png to get and display the default sprite
-                self.pokemonImageView.sd_setImage(with: URL(string: self.imageAPIBaseURL + "\(json["id"])" + ".png"), completed: nil)
+                self.pokemonImageView.sd_setImage(with: URL(string: self.imageAPIBaseURL + json["id"].stringValue + ".png"), completed: nil)
+                let pokemonType = (json["types"].arrayValue)[0]["type"]["name"].stringValue
+                print(pokemonType)
+                switch pokemonType {
+                case "normal":
+                    self.mainView.backgroundColor = UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
+                case "fire":
+                    self.mainView.backgroundColor = UIColor(red: 238 / 255, green: 128 / 255, blue:  59 / 255, alpha: 1)
+                case "fighting":
+                    self.mainView.backgroundColor = UIColor(red: 190 / 255, green:  49 / 255, blue:  45 / 255, alpha: 1)
+                case "water":
+                    self.mainView.backgroundColor = UIColor(red: 106 / 255, green: 146 / 255, blue: 237 / 255, alpha: 1)
+                case "flying":
+                    self.mainView.backgroundColor = UIColor(red: 168 / 255, green: 147 / 255, blue: 237 / 255, alpha: 1)
+                case "grass":
+                    self.mainView.backgroundColor = UIColor(red: 123 / 255, green: 198 / 255, blue:  87 / 255, alpha: 1)
+                case "poison":
+                    self.mainView.backgroundColor = UIColor(red: 159 / 255, green:  68 / 255, blue: 158 / 255, alpha: 1)
+                case "electric":
+                    self.mainView.backgroundColor = UIColor(red: 247 / 255, green: 206 / 255, blue:  67 / 255, alpha: 1)
+                case "ground":
+                    self.mainView.backgroundColor = UIColor(red: 223 / 255, green: 191 / 255, blue: 110 / 255, alpha: 1)
+                case "psychic":
+                    self.mainView.backgroundColor = UIColor(red: 246 / 255, green:  91 / 255, blue: 137 / 255, alpha: 1)
+                case "rock":
+                    self.mainView.backgroundColor = UIColor(red: 183 / 255, green: 159 / 255, blue:  65 / 255, alpha: 1)
+                case "ice":
+                    self.mainView.backgroundColor = UIColor(red: 154 / 255, green: 216 / 255, blue: 215 / 255, alpha: 1)
+                case "bug":
+                    self.mainView.backgroundColor = UIColor(red: 168 / 255, green: 182 / 255, blue:  49 / 255, alpha: 1)
+                case "dragon":
+                    self.mainView.backgroundColor = UIColor(red: 112 / 255, green:  67 / 255, blue: 244 / 255, alpha: 1)
+                case "ghost":
+                    self.mainView.backgroundColor = UIColor(red: 112 / 255, green:  90 / 255, blue: 150 / 255, alpha: 1)
+                case "dark":
+                    self.mainView.backgroundColor = UIColor(red: 112 / 255, green:  88 / 255, blue:  73 / 255, alpha: 1)
+                case "steel":
+                    self.mainView.backgroundColor = UIColor(red: 184 / 255, green: 184 / 255, blue: 207 / 255, alpha: 1)
+                case "fairy":
+                    self.mainView.backgroundColor = UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
+                default:
+                    print("This should never appear")
+                }
             case.failure(let error):
                 //Displays an error if it fails
                 self.descriptionTextView.text = "Pokemon not found!"
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    enum PokemonType: String {
+        case normal = "normal" //UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
+        case fire = "fire" //- UIColor(red: 238 / 255, green: 128 / 255, blue:  59 / 255, alpha: 1)
+        case fighting = "fighting" //- UIColor(red: 190 / 255, green:  49 / 255, blue:  45 / 255, alpha: 1)
+        case water = "water" //- UIColor(red: 106 / 255, green: 146 / 255, blue: 237 / 255, alpha: 1)
+        case flying = "flying" //- UIColor(red: 168 / 255, green: 147 / 255, blue: 237 / 255, alpha: 1)
+        case grass = "grass" //- UIColor(red: 123 / 255, green: 198 / 255, blue:  87 / 255, alpha: 1)
+        case poison = "poison" //- UIColor(red: 159 / 255, green:  68 / 255, blue: 158 / 255, alpha: 1)
+        case electric = "electric" //- UIColor(red: 247 / 255, green: 206 / 255, blue:  67 / 255, alpha: 1)
+        case ground = "ground" //- UIColor(red: 223 / 255, green: 191 / 255, blue: 110 / 255, alpha: 1)
+        case psychic = "psychic" //- UIColor(red: 246 / 255, green:  91 / 255, blue: 137 / 255, alpha: 1)
+        case rock = "rock" //- UIColor(red: 183 / 255, green: 159 / 255, blue:  65 / 255, alpha: 1)
+        case ice = "ice" //- UIColor(red: 154 / 255, green: 216 / 255, blue: 215 / 255, alpha: 1)
+        case bug  = "bug" //- UIColor(red: 168 / 255, green: 182 / 255, blue:  49 / 255, alpha: 1)
+        case dragon = "dragon" //- UIColor(red: 112 / 255, green:  67 / 255, blue: 244 / 255, alpha: 1)
+        case ghost = "ghost" //- UIColor(red: 112 / 255, green:  90 / 255, blue: 150 / 255, alpha: 1)
+        case dark = "dark" //- UIColor(red: 112 / 255, green:  88 / 255, blue:  73 / 255, alpha: 1)
+        case steel = "steel" //- UIColor(red: 184 / 255, green: 184 / 255, blue: 207 / 255, alpha: 1)
+        case fairy = "fairy" //- UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
     }
     
 
