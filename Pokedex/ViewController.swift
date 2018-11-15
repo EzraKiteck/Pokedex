a2f0c235e4fa22fbd0482e3175232a6150aa8ff2
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-15 09:52:20 -0600

    Removed commented out code

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -103,62 +103,53 @@ class ViewController: UIViewController {
                 default:
                     print("This should never appear")
                 }
-                //Gets second pokemonType and changes bg color
+                //Gets second pokemonType (if it exists) and changes bg color
                 if (json["types"].arrayValue).count == 2 {
                     self.pokemonType2 = (json["types"].arrayValue)[1]["type"]["name"].stringValue
                 } else {
                     self.pokemonType2 = "default"
                 }
-                print(self.pokemonType2)
-                /*if json["types"].arrayValue)[1]["type"]["name"].stringValue != nil {
-                    var pokemonType2 = json["types"].arrayValue)[1]["type"]["name"].stringValue
-                } else {
-                    self.descriptionTextView.backgroundColor = .white
-                }*/
-                if self.pokemonType2 == "" {
-                    self.descriptionTextView.backgroundColor = .white
-                } else {
-                    switch self.pokemonType2 {
-                    case "normal":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
-                    case "fire":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 238 / 255, green: 128 / 255, blue:  59 / 255, alpha: 1)
-                    case "fighting":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 190 / 255, green:  49 / 255, blue:  45 / 255, alpha: 1)
-                    case "water":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 106 / 255, green: 146 / 255, blue: 237 / 255, alpha: 1)
-                    case "flying":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 147 / 255, blue: 237 / 255, alpha: 1)
-                    case "grass":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 123 / 255, green: 198 / 255, blue:  87 / 255, alpha: 1)
-                    case "poison":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 159 / 255, green:  68 / 255, blue: 158 / 255, alpha: 1)
-                    case "electric":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 247 / 255, green: 206 / 255, blue:  67 / 255, alpha: 1)
-                    case "ground":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 223 / 255, green: 191 / 255, blue: 110 / 255, alpha: 1)
-                    case "psychic":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 246 / 255, green:  91 / 255, blue: 137 / 255, alpha: 1)
-                    case "rock":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 183 / 255, green: 159 / 255, blue:  65 / 255, alpha: 1)
-                    case "ice":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 154 / 255, green: 216 / 255, blue: 215 / 255, alpha: 1)
-                    case "bug":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 182 / 255, blue:  49 / 255, alpha: 1)
-                    case "dragon":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  67 / 255, blue: 244 / 255, alpha: 1)
-                    case "ghost":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  90 / 255, blue: 150 / 255, alpha: 1)
-                    case "dark":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  88 / 255, blue:  73 / 255, alpha: 1)
-                    case "steel":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 184 / 255, green: 184 / 255, blue: 207 / 255, alpha: 1)
-                    case "fairy":
-                        self.descriptionTextView.backgroundColor = UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
-                    default:
-                        self.descriptionTextView.backgroundColor = self.mainView.backgroundColor
-                    }
+                switch self.pokemonType2 {
+                case "normal":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
+                case "fire":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 238 / 255, green: 128 / 255, blue:  59 / 255, alpha: 1)
+                case "fighting":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 190 / 255, green:  49 / 255, blue:  45 / 255, alpha: 1)
+                case "water":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 106 / 255, green: 146 / 255, blue: 237 / 255, alpha: 1)
+                case "flying":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 147 / 255, blue: 237 / 255, alpha: 1)
+                case "grass":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 123 / 255, green: 198 / 255, blue:  87 / 255, alpha: 1)
+                case "poison":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 159 / 255, green:  68 / 255, blue: 158 / 255, alpha: 1)
+                case "electric":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 247 / 255, green: 206 / 255, blue:  67 / 255, alpha: 1)
+                case "ground":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 223 / 255, green: 191 / 255, blue: 110 / 255, alpha: 1)
+                case "psychic":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 246 / 255, green:  91 / 255, blue: 137 / 255, alpha: 1)
+                case "rock":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 183 / 255, green: 159 / 255, blue:  65 / 255, alpha: 1)
+                case "ice":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 154 / 255, green: 216 / 255, blue: 215 / 255, alpha: 1)
+                case "bug":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 182 / 255, blue:  49 / 255, alpha: 1)
+                case "dragon":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  67 / 255, blue: 244 / 255, alpha: 1)
+                case "ghost":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  90 / 255, blue: 150 / 255, alpha: 1)
+                case "dark":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  88 / 255, blue:  73 / 255, alpha: 1)
+                case "steel":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 184 / 255, green: 184 / 255, blue: 207 / 255, alpha: 1)
+                case "fairy":
+                    self.descriptionTextView.backgroundColor = UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
+                default:
+                    self.descriptionTextView.backgroundColor = self.mainView.backgroundColor
                 }
+                
             case.failure(let error):
                 //Displays an error if it fails
                 self.descriptionTextView.text = "Pokemon not found!"
