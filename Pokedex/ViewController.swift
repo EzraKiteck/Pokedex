9c319743832b85f331d2e9855703dede0c3881c2
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-11-15 09:49:56 -0600

    The text view will change color to the second type of a pokemon, and if the pokemon only has one type, it will be the same color as the mainView bg color

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var pokemonImageView: UIImageView!
     @IBOutlet var mainView: UIView!
     
+    var pokemonName = ""
+    var pokemonType2 = ""
+    
     //Base URL for request
     let pokemonAPIBaseURL = "https://pokeapi.co/api/v2/pokemon/"
     let imageAPIBaseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
@@ -34,8 +37,10 @@ class ViewController: UIViewController {
         pokemonNameTextField.resignFirstResponder()
         
         //Checks against the Pokemon name being nil
-        guard let pokemonName = pokemonNameTextField.text else {
-                return
+        if pokemonNameTextField.text == "" {
+            return
+        } else {
+            pokemonName = pokemonNameTextField.text!
         }
         
         //Clears text field
@@ -56,8 +61,8 @@ class ViewController: UIViewController {
                 self.descriptionTextView.text = "Name: \(json["name"])\nID: \(json["id"])"
                 //Uses the base URL + the pokemons ID# + .png to get and display the default sprite
                 self.pokemonImageView.sd_setImage(with: URL(string: self.imageAPIBaseURL + json["id"].stringValue + ".png"), completed: nil)
+                //Gets first pokemonType and changes bg color
                 let pokemonType = (json["types"].arrayValue)[0]["type"]["name"].stringValue
-                print(pokemonType)
                 switch pokemonType {
                 case "normal":
                     self.mainView.backgroundColor = UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
@@ -97,6 +102,62 @@ class ViewController: UIViewController {
                     self.mainView.backgroundColor = UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
                 default:
                     print("This should never appear")
+                }
+                //Gets second pokemonType and changes bg color
+                if (json["types"].arrayValue).count == 2 {
+                    self.pokemonType2 = (json["types"].arrayValue)[1]["type"]["name"].stringValue
+                } else {
+                    self.pokemonType2 = "default"
+                }
+                print(self.pokemonType2)
+                /*if json["types"].arrayValue)[1]["type"]["name"].stringValue != nil {
+                    var pokemonType2 = json["types"].arrayValue)[1]["type"]["name"].stringValue
+                } else {
+                    self.descriptionTextView.backgroundColor = .white
+                }*/
+                if self.pokemonType2 == "" {
+                    self.descriptionTextView.backgroundColor = .white
+                } else {
+                    switch self.pokemonType2 {
+                    case "normal":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 167 / 255, blue: 122 / 255, alpha: 1)
+                    case "fire":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 238 / 255, green: 128 / 255, blue:  59 / 255, alpha: 1)
+                    case "fighting":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 190 / 255, green:  49 / 255, blue:  45 / 255, alpha: 1)
+                    case "water":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 106 / 255, green: 146 / 255, blue: 237 / 255, alpha: 1)
+                    case "flying":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 147 / 255, blue: 237 / 255, alpha: 1)
+                    case "grass":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 123 / 255, green: 198 / 255, blue:  87 / 255, alpha: 1)
+                    case "poison":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 159 / 255, green:  68 / 255, blue: 158 / 255, alpha: 1)
+                    case "electric":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 247 / 255, green: 206 / 255, blue:  67 / 255, alpha: 1)
+                    case "ground":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 223 / 255, green: 191 / 255, blue: 110 / 255, alpha: 1)
+                    case "psychic":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 246 / 255, green:  91 / 255, blue: 137 / 255, alpha: 1)
+                    case "rock":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 183 / 255, green: 159 / 255, blue:  65 / 255, alpha: 1)
+                    case "ice":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 154 / 255, green: 216 / 255, blue: 215 / 255, alpha: 1)
+                    case "bug":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 168 / 255, green: 182 / 255, blue:  49 / 255, alpha: 1)
+                    case "dragon":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  67 / 255, blue: 244 / 255, alpha: 1)
+                    case "ghost":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  90 / 255, blue: 150 / 255, alpha: 1)
+                    case "dark":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 112 / 255, green:  88 / 255, blue:  73 / 255, alpha: 1)
+                    case "steel":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 184 / 255, green: 184 / 255, blue: 207 / 255, alpha: 1)
+                    case "fairy":
+                        self.descriptionTextView.backgroundColor = UIColor(red: 236 / 255, green: 154 / 255, blue: 172 / 255, alpha: 1)
+                    default:
+                        self.descriptionTextView.backgroundColor = self.mainView.backgroundColor
+                    }
                 }
             case.failure(let error):
                 //Displays an error if it fails
